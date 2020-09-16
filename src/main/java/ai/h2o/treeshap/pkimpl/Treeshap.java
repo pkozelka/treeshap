bd2bf8e64c32316c43a76921fca70f7b3ad81d33
Petr Kozelka <pkozelka@gmail.com>
2020-09-16 23:41:11 +0200

    initial maven project

---
+++ src/main/java/ai/h2o/treeshap/pkimpl/Treeshap.java
@@ -0,0 +1,7 @@
+package ai.h2o.treeshap.pkimpl;
+
+public class Treeshap {
+    public static void main(String... args) {
+        System.out.printf("Hello from %s%n", Treeshap.class.getName());
+    }
+}
---
+++ src/test/java/ai/h2o/treeshap/pkimpl/TreeshapTest.java
@@ -0,0 +1,11 @@
+package ai.h2o.treeshap.pkimpl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TreeshapTest {
+    @Test
+    public void testSomething() {
+        Assert.assertEquals(2,1+1);
+    }
+}
